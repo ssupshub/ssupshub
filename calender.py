37d3e12fd9228759d2aa9e3f35e2cb8e1d4fd23d
shubham <140893209+ssupshub@users.noreply.github.com>
2024-08-13 00:12:46 +0530

    raw

---
+++ calender.py
@@ -0,0 +1,23 @@
+import calendar
+year=2022
+month=3
+x=calendar.month(year,month)
+print(x)
+
+import calendar
+year=2005
+month=10
+x=calendar.month(year,month)
+print(x) 
+
+import calendar
+year=2005
+month=8
+x=calendar.month(year,month)
+print(x)
+
+import calculator
+year=2022
+month=10
+x=calendar .month(year,month)
+print(x) 
